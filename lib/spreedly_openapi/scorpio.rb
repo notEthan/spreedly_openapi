c847a4c953ee41d3bbd9e34107d58569aa30b331
Ethan <ethan@unth.net>
2020-02-19 08:18:19 -0800

    jsi_schema_module

--- lib/spreedly_openapi/scorpio.rb
+++ lib/spreedly_openapi/scorpio.rb
@@ -2,29 +2,29 @@ require 'scorpio'
 require 'spreedly_openapi'
 
 module SpreedlyOpenAPI
-  Document = Scorpio::OpenAPI::V3::Document.new(::YAML.load(SPREEDLY_OPENAPI3_YML))
+  Document = Scorpio::OpenAPI::Document.from_instance(::YAML.load(SPREEDLY_OPENAPI3_YML))
 
   # GENERATED (relying on activesupport) vaguely like
   # puts SpreedlyOpenAPI::Document.components.schemas.select { |k,v| ['object', nil].include?(v['type']) }.keys.map { |k| "#{k.camelize} = ...('#{k}')" }
 
-  Errors            = JSI.class_for_schema(Document.components.schemas['errors'])
-  TransactionWrapper = JSI.class_for_schema(Document.components.schemas['transaction_wrapper'])
-  Transaction        = JSI.class_for_schema(Document.components.schemas['transaction'])
-  Gateway             = JSI.class_for_schema(Document.components.schemas['gateway'])
-  GatewayCreateRequest = JSI.class_for_schema(Document.components.schemas['gateway_create_request'])
-  GatewayUpdateRequest  = JSI.class_for_schema(Document.components.schemas['gateway_update_request'])
-  ReceiverOption        = JSI.class_for_schema(Document.components.schemas['receiver_option'])
-  ReceiverCreateRequest = JSI.class_for_schema(Document.components.schemas['receiver_create_request'])
-  ReceiverCredentials  = JSI.class_for_schema(Document.components.schemas['receiver_credentials'])
-  Receiver            = JSI.class_for_schema(Document.components.schemas['receiver'])
-  PaymentMethod        = JSI.class_for_schema(Document.components.schemas['payment_method'])
-  PassInCreditCard      = JSI.class_for_schema(Document.components.schemas['pass_in_credit_card'])
-  PassInBankAccount      = JSI.class_for_schema(Document.components.schemas['pass_in_bank_account'])
-  PassInAndroidPay        = JSI.class_for_schema(Document.components.schemas['pass_in_android_pay'])
-  PassInGooglePay          = JSI.class_for_schema(Document.components.schemas['pass_in_google_pay'])
-  PassInApplePay            = JSI.class_for_schema(Document.components.schemas['pass_in_apple_pay'])
-  PurchaseTransactionRequest = JSI.class_for_schema(Document.components.schemas['purchase_transaction_request'])
-  Certificate               = JSI.class_for_schema(Document.components.schemas['certificate'])
+  Errors            = Document.components.schemas['errors'].jsi_schema_module
+  TransactionWrapper = Document.components.schemas['transaction_wrapper'].jsi_schema_module
+  Transaction        = Document.components.schemas['transaction'].jsi_schema_module
+  Gateway             = Document.components.schemas['gateway'].jsi_schema_module
+  GatewayCreateRequest = Document.components.schemas['gateway_create_request'].jsi_schema_module
+  GatewayUpdateRequest  = Document.components.schemas['gateway_update_request'].jsi_schema_module
+  ReceiverOption        = Document.components.schemas['receiver_option'].jsi_schema_module
+  ReceiverCreateRequest = Document.components.schemas['receiver_create_request'].jsi_schema_module
+  ReceiverCredentials  = Document.components.schemas['receiver_credentials'].jsi_schema_module
+  Receiver            = Document.components.schemas['receiver'].jsi_schema_module
+  PaymentMethod        = Document.components.schemas['payment_method'].jsi_schema_module
+  PassInCreditCard      = Document.components.schemas['pass_in_credit_card'].jsi_schema_module
+  PassInBankAccount      = Document.components.schemas['pass_in_bank_account'].jsi_schema_module
+  PassInAndroidPay        = Document.components.schemas['pass_in_android_pay'].jsi_schema_module
+  PassInGooglePay          = Document.components.schemas['pass_in_google_pay'].jsi_schema_module
+  PassInApplePay            = Document.components.schemas['pass_in_apple_pay'].jsi_schema_module
+  PurchaseTransactionRequest = Document.components.schemas['purchase_transaction_request'].jsi_schema_module
+  Certificate               = Document.components.schemas['certificate'].jsi_schema_module
 
   # @param request [Scorpio::Request]
   # @param resource_name [String]
@@ -55,7 +55,7 @@ module SpreedlyOpenAPI
     nil
   end
 
-  class Receiver
+  module Receiver
     # @return [SpreedlyOpenAPI::Receiver]
     def self.find_or_create(receiver_type: , hostnames: , credentials: , **request_config)
       find(receiver_type: receiver_type, hostnames: hostnames, credentials: credentials, **request_config) ||
